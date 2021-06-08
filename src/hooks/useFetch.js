640f054cc224c1938091afeba3fe52b50ff78863
Paul Cortes Renteria <pcortesr@psl.com.co>
2021-06-07 20:03:18 -0500

    useRef ejemplo real

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,11 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const useFetch = (url) => {
+  const isMounted = useRef(true);
+
   const [state, setState] = useState({
     data: null,
     loading: true,
     error: null,
   });
+
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
 
   useEffect(() => {
     setState({
@@ -16,11 +24,13 @@ export const useFetch = (url) => {
     fetch(url)
       .then((resp) => resp.json())
       .then((data) => {
-        setState({
-          loading: false,
-          error: null,
-          data,
-        });
+        if (isMounted.current) {
+          setState({
+            loading: false,
+            error: null,
+            data,
+          });
+        }
       });
   }, [url]);
 
