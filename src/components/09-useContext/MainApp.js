e1bcf0b923a6e4bf3a185122a3470de765be604c
Paul Cortes Renteria <pcortesr@psl.com.co>
2021-06-10 16:12:29 -0500

    Creacion de UserContext

--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
 
 export const MainApp = () => {
+  
+  const user = {
+    id: 123,
+    name:'Paul',
+    email:'paul@paul.com'
+  }
   return (
+    <UserContext.Provider value={ user }>
       <AppRouter />
-  )
+    </UserContext.Provider>
+  );
 }
